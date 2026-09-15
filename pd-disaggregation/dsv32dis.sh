d0436017b08a1fabc813f943a21ec17d466fbdfa
hibikid <7514545+hibikid@user.noreply.gitee.com>
2026-09-15 18:20:45 +0800

    all script

---
+++ pd-disaggregation/dsv32dis.sh
@@ -0,0 +1,132 @@
+# cpu高性能
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+# 绑核
+export SGLANG_SET_CPU_AFFINITY=1
+# 设置PYTHONPATH
+cd /home/cryang_wx1511021/sglang
+export PYTHONPATH=${PWD}/python:$PYTHONPATH
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+unset ASCEND_LAUNCH_BLOCKING
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}
+export PATH=/usr/local/Ascend/8.5.0/compiler/bishengir/bin:$PATH
+# 内存碎片
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+export STREAMS_PER_DEVICE=32
+# pd传输, IP设置为p节点首节点
+export USE_VLLM_CUSTOM_ALLREDUCE=1
+export ASCEND_MF_TRANSFER_PROTOCOL="device_rdma"
+export ASCEND_MF_STORE_URL="tcp://61.28.30.27:24670"
+# p节点IP
+P_IP=('61.28.30.27')
+# D节点IP D节点首节点IP
+D_IP=('61.28.30.28')
+#MODEL_PATH=/mnt/share/weights/DeepSeek-V3.2-1201-w8a8/
+#MODEL_PATH=/mnt/share/DeepSeek-V3.1-Terminus-w8a8-QuaRot-lfs
+#MODEL_PATH=/mnt/share/l00890003/DeepSeek-V3.2-1201-w8a8
+MODEL_PATH=/home/cryang_wx1511021/DeepSeek-V3.2-Exp-w8a8
+# enable mlapo
+#export SGLANG_NPU_USE_MLAPO=1
+#export SGLANG_USE_FIA_NZ=1
+#export SGLANG_NPU_USE_MULTI_STREAM=1
+LOCAL_HOST1='61.28.30.27'
+echo "${LOCAL_HOST1}"
+
+export SGLANG_ENABLE_SPARSITY_DRIVEN_KV_OFFLOAD=1
+export SGLANG_CUDA_COREDUMP_BEFORE_CRASH=0
+export CUDA_ENABLE_COREDUMP_ON_EXCEPTION=0
+export CUDA_ENABLE_USER_TRIGGERED_COREDUMP=0
+unset CUDA_COREDUMP_FILE
+unset CUDA_COREDUMP_PIPE
+
+#export USE_DEEPEP_INT8=1
+# prefill
+for i in "${!P_IP[@]}";
+do
+    if [[ "$LOCAL_HOST1" == "${P_IP[$i]}" ]];
+    then
+        echo "${P_IP[$i]}"
+        export HCCL_BUFFSIZE=900
+        export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
+        export TASK_QUEUE_ENABLE=2
+        export HCCL_SOCKET_IFNAME=enp196s0f0
+        export GLOO_SOCKET_IFNAME=enp196s0f0
+        python3 -m sglang.launch_server --model-path ${MODEL_PATH} \
+        --tp 16 \
+        --base-gpu-id 0 \
+        --gpu-id-step 1 \
+        --trust-remote-code \
+        --attention-backend ascend \
+        --device npu \
+        --watchdog-timeout 9000 \
+        --host ${P_IP[$i]} --port 8000 \
+        --mem-fraction-static 0.80 \
+        --context-length 64000 \
+        --disable-radix-cache --chunked-prefill-size -1 --max-prefill-tokens 64000 \
+        --max-running-requests 4 \
+        --prefill-max-requests 1 \
+        --quantization modelslim \
+        --disaggregation-transfer-backend ascend \
+        --disaggregation-mode prefill \
+        --disable-cuda-graph \
+        --nnodes 1 --node-rank 0 \
+        --disaggregation-bootstrap-port 8995 \
+        --moe-dense-tp-size 1 \
+        --dist-init-addr ${P_IP[0]}:10000
+        break
+    fi
+done
+#MODEL_PATH=/mnt/share/weights/DeepSeek-V3/DeepSeek-V3-w8a8_mix_mtp
+# decode
+for i in "${!D_IP[@]}";
+do
+    if [[ "$LOCAL_HOST1" == "${D_IP[$i]}" ]];
+    then
+        echo "${D_IP[$i]}"
+        export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+        export SGLANG_ENABLE_SPEC_V2=1
+        export HCCL_BUFFSIZE=900
+#       export ENABLE_PROFILING=1
+#       export SGLANG_DP_ROUND_ROBIN=1
+        export TASK_QUEUE_ENABLE=0
+        export SGLANG_SCHEDULER_SKIP_ALL_GATHER=1
+#       export ENABLE_FUSED_MOE=1
+        export HCCL_SOCKET_IFNAME=enp196s0f0
+        export GLOO_SOCKET_IFNAME=enp196s0f0
+        export HCCL_BUFFSIZE=900
+        export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=8
+        python3 -m sglang.launch_server --model-path ${MODEL_PATH} \
+        --tp 16 \
+        --dp 1 \
+        --base-gpu-id 0 \
+        --gpu-id-step 1 \
+        --trust-remote-code \
+        --attention-backend ascend \
+        --device npu \
+        --watchdog-timeout 9000 \
+        --host ${D_IP[$i]} --port 8001 \
+        --mem-fraction-static 0.80 \
+        --context-length 64000 \
+        --disable-radix-cache \
+        --chunked-prefill-size -1 --max-prefill-tokens 64000 \
+        --max-running-requests 4 \
+        --prefill-max-requests 1 \
+        --cuda-graph-max-bs 4 \
+        --quantization modelslim \
+        --disaggregation-transfer-backend ascend \
+        --disaggregation-mode decode \
+        --disaggregation-decode-extra-slots 0 \
+        --nnodes 1 --node-rank 0 \
+        --prefill-round-robin-balance \
+        --dist-init-addr ${D_IP[0]}:10000
+        break
+    fi
+done
+exit 1
---
+++ sglang-docker-run.sh
@@ -0,0 +1,16 @@
+sudo docker run -itd \
+  --name sglang0516_npu_cryang \
+  --shm-size=512g \
+  --privileged \
+  --net=host \
+  -v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+  -v /etc/ascend_install.info:/etc/ascend_install.info \
+  -v /etc/hccn.conf:/etc/hccn.conf \
+  -v /etc/localtime:/etc/localtime \
+  -v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi \
+  --device=/dev/davinci_manager \
+  --device=/dev/devmm_svm \
+  --device=/dev/hisi_hdc \
+  -v /home/youhuibai/cryang:/home/cryang \
+  quay.io/ascend/sglang:v0.5.16-cann9.0.0-a3 \
+  /bin/bash
